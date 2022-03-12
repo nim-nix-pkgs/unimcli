89a251abc95d57b35f072eaaf1b0c26cd981a6a5
hugosenari <hugosenari@gmail.com>
2022-03-12 01:45:05 -0300

    chore: re index unimcli

---
+++ master/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = ''User-friendly nimcli.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-unimcli-master.flake = false;
+  inputs.src-unimcli-master.owner = "unimorg";
+  inputs.src-unimcli-master.ref   = "refs/heads/master";
+  inputs.src-unimcli-master.repo  = "unimcli";
+  inputs.src-unimcli-master.type  = "github";
+  
+  inputs."cligen".dir   = "nimpkgs/c/cligen";
+  inputs."cligen".owner = "riinr";
+  inputs."cligen".ref   = "flake-pinning";
+  inputs."cligen".repo  = "flake-nimble";
+  inputs."cligen".type  = "github";
+  inputs."cligen".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cligen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimble".dir   = "nimpkgs/n/nimble";
+  inputs."nimble".owner = "riinr";
+  inputs."nimble".ref   = "flake-pinning";
+  inputs."nimble".repo  = "flake-nimble";
+  inputs."nimble".type  = "github";
+  inputs."nimble".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimble".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-unimcli-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-unimcli-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
