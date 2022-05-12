7bdfc97b4d5231762f53aae77231a8c63988c50f
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:30 -0300

    chore: re index unimcli

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."unimcli-master".dir   = "master";
+  inputs."unimcli-master".owner = "nim-nix-pkgs";
+  inputs."unimcli-master".ref   = "master";
+  inputs."unimcli-master".repo  = "unimcli";
+  inputs."unimcli-master".type  = "github";
+  inputs."unimcli-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."unimcli-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
